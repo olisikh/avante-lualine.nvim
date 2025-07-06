fc7db5d85bb3c12912a0e463627826ee5358b71c
Oleksii Lisikh <alisiikh@gmail.com>
2025-07-06 23:51:58 +0200

    Trying something out

--- lua/lualine/components/avante.lua
+++ lua/lualine/components/avante.lua
@@ -1,7 +1,7 @@
 return function()
-    local ok, avante = pcall(require, "avante.config")
-    if not ok or not avante or not avante.provider then
-        return "Avante: N/A"
-    end
-    return "Avante: " .. avante.config.provider
+  local ok, config = pcall(require, "avante.config")
+  if not ok or type(config.provider) ~= "string" then
+    return "Avante: N/A"
+  end
+  return "Avante: " .. config.provider
 end
