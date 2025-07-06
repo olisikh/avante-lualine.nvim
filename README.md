ad19c9b59b6eb0f8565b23c1bcb3c34c00a2d88c
Oleksii Lisikh <alisiikh@gmail.com>
2025-07-07 00:29:37 +0200

    Write some README

---
+++ README.md
@@ -0,0 +1,19 @@
+## Avante plugin for lualine.nvim
+
+This is a minimalistic plugin for showing the Avante provider in your [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim).
+
+Dependencies: [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim), [avante.nvim](https://github.com/yetone/avante.nvim)
+
+
+## Installation
+Here is how you can add it to your lualine:
+
+```lua
+lualine_x = [
+  ...
+  'avante',
+  ...
+]
+```
+
+
--- lua/lualine/components/avante.lua
+++ lua/lualine/components/avante.lua
@@ -1,7 +1,20 @@
 return function()
-  local ok, config = pcall(require, "avante.config")
-  if not ok or type(config.provider) ~= "string" then
-    return "Avante: N/A"
-  end
-  return "Avante: " .. config.provider
+    local ok, config = pcall(require, "avante.config")
+    if not ok or type(config.provider) ~= "string" then
+        return "Avante:  "
+    end
+
+    if config.provider == "claude" then
+        return "Avante: 󰛄 "
+    elseif config.provider == "openai" then
+        return "Avante: 󰶘 "
+    elseif config.provider == "azure" then
+        return "Avante:  "
+    elseif config.provider == "gemini" then
+        return "Avante:  "
+    elseif config.provider == "copilot" then
+        return "Avante:  "
+    end
+
+    return "Avante: " .. config.provider
 end
