421d0c9119f12a397298f2a746b2007901f39993
Oleksii Lisikh <alisiikh@gmail.com>
2025-07-06 23:40:19 +0200

    Make it dead simple

--- lua/avante-lualine/lualine/components/avante.lua
+++ lua/avante-lualine/lualine/components/avante.lua
@@ -1,7 +1,7 @@
 return function()
     local ok, avante = pcall(require, "avante.config")
     if not ok or not avante or not avante.provider then
-        return ""
+        return "Avante: N/A"
     end
     return "Avante: " .. avante.config.provider
 end
